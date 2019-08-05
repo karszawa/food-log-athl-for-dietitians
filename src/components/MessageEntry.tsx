bb21302e74225190ad743927563d607de68f219a
Hiroaki KARASAWA <karszawa@mercari.com>
2019-08-05 23:14:54 +0900

    chore: modify message space

--- src/components/MessageEntry.tsx
+++ src/components/MessageEntry.tsx
@@ -4,23 +4,31 @@ import { Message } from "../lib/firestore.d";
 
 interface Props {
   message: Message;
+  athleteId: string;
 }
 
 export const MessageEntry = (props: Props) => {
-  const { message } = props;
+  const { message, athleteId } = props;
+  const isFromAthlete = message.from === athleteId;
 
   return (
-    <Container>
+    <Container isFromAthlete={isFromAthlete}>
       <Text>{message.text}</Text>
     </Container>
   );
 };
+
+interface ContainerProps {
+  isFromAthlete: boolean;
+}
 
 const Container = styled.View`
   background-color: white;
   border-radius: 8px;
   max-width: 80%;
   padding: 6px 8px;
+  ${(props: ContainerProps) =>
+    props.isFromAthlete ? "margin-right: auto;" : "margin-left: auto;"}
 `;
 
 const Text = styled.Text`
