ee3045e908f6161763a2d945e00e681e04782097
Hiroaki KARASAWA <karszawa@mercari.com>
2019-08-11 03:29:43 +0900

    feat: load font while app loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { Provider } from "react-redux";
+import { AppLoading, Font } from "expo";
 import Navigator from "./navigation";
 import { createStore } from "./store";
 
 const store = createStore();
 
-export default () => (
-  <Provider store={store}>
-    <Navigator />
-  </Provider>
-);
+export default () => {
+  const { isReady, finish, setup } = useSetup();
+
+  if (!isReady) {
+    return <AppLoading startAsync={setup} onFinish={finish} />;
+  }
+
+  return (
+    <Provider store={store}>
+      <Navigator />
+    </Provider>
+  );
+};
+
+const useSetup = () => {
+  const [isReady, setIsReady] = useState(false);
+  const setup = Font.loadAsync({
+    Roboto: require("./node_modules/native-base/Fonts/Roboto.ttf"),
+    Roboto_medium: require("./node_modules/native-base/Fonts/Roboto_medium.ttf"),
+  });
+  const finish = useCallback(() => {
+    setIsReady(true);
+  }, [setIsReady]);
+
+  return {
+    isReady,
+    finish,
+    setup,
+  };
+};
